25b6f64d0a70503e9272eeda8efce3beb6994369
Georgi Minchev <georgiminchev1206@gmail.com>
2024-06-06 16:07:57 +0300

    Add 4 Buttons

--- Draftsman/Form1.Designer.cs
+++ Draftsman/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Draftsman
 {
-    partial class Form1
+    partial class Board
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnRectangle = new Button();
+            btnTriangle = new Button();
+            btnCircle = new Button();
+            btnReset = new Button();
+            SuspendLayout();
+            // 
+            // btnRectangle
+            // 
+            btnRectangle.Cursor = Cursors.Hand;
+            btnRectangle.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnRectangle.Location = new Point(12, 12);
+            btnRectangle.Name = "btnRectangle";
+            btnRectangle.Size = new Size(168, 67);
+            btnRectangle.TabIndex = 0;
+            btnRectangle.Text = "Rectangle";
+            btnRectangle.UseVisualStyleBackColor = true;
+            // 
+            // btnTriangle
+            // 
+            btnTriangle.Cursor = Cursors.Hand;
+            btnTriangle.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTriangle.Location = new Point(12, 85);
+            btnTriangle.Name = "btnTriangle";
+            btnTriangle.Size = new Size(168, 67);
+            btnTriangle.TabIndex = 1;
+            btnTriangle.Text = "Triangle";
+            btnTriangle.UseVisualStyleBackColor = true;
+            // 
+            // btnCircle
+            // 
+            btnCircle.Cursor = Cursors.Hand;
+            btnCircle.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCircle.Location = new Point(12, 158);
+            btnCircle.Name = "btnCircle";
+            btnCircle.Size = new Size(168, 67);
+            btnCircle.TabIndex = 2;
+            btnCircle.Text = "Circle";
+            btnCircle.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            btnReset.Cursor = Cursors.Hand;
+            btnReset.Font = new Font("Showcard Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnReset.Location = new Point(92, 231);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(88, 28);
+            btnReset.TabIndex = 3;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            // 
+            // Board
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReset);
+            Controls.Add(btnCircle);
+            Controls.Add(btnTriangle);
+            Controls.Add(btnRectangle);
+            Name = "Board";
+            Text = "Board";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnRectangle;
+        private Button btnTriangle;
+        private Button btnCircle;
+        private Button btnReset;
     }
 }
